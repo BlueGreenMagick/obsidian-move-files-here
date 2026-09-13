e8280be3cd05f66a51cc288cdad03c5477904bea
Yoonchae Lee <hello@yoonchae.com>
2026-09-13 21:51:14 +0900

    add error handler

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
 import * as Obsidian from "obsidian";
 import { addMoveSelectedItems } from "./move-menu";
 
-export class MoveFilesHerePlugin extends Obsidian.Plugin {
+export class SelectFileForMovePlugin extends Obsidian.Plugin {
   onload(): void {
     this.registerEvent(
       this.app.workspace.on("file-menu", (menu, file, source) => {
-        if (source === "file-explorer-context-menu" && file instanceof Obsidian.TFolder) {
-          addMoveSelectedItems(this.app, menu, file);
+        if (
+          source === "file-explorer-context-menu" &&
+          file instanceof Obsidian.TFolder
+        ) {
+          try {
+            addMoveSelectedItems(this.app, menu, file);
+          } catch (error) {
+            console.error(
+              "Move Files Here: failed to add context menu.",
+              error,
+            );
+            new Obsidian.Notice(
+              "Move Files Here: failed to add context menu. Open dev tools for detailed error message.",
+            );
+          }
         }
       }),
     );
   }
 }
 
-export default MoveFilesHerePlugin;
+export default SelectFileForMovePlugin;
